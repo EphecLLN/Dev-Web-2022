8ffb7d7a91a3da5845ab7280e8988daf37e407ef
WassimBouhdid <HE201893@students.ephec.be>
2022-02-20 18:20:01 +0100

    installation de package et configuration d'expressjs

---
+++ serveur/src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan')
+
+const app = express();
+
+app.use(bodyParser.json())
+app.use(cors())
+app.use(morgan('common'))
+const port = process.env.port || 8080;
+
+app.listen(port,() => console.log(`server started on port ${port}`));
+
+app.get("/",(req,res) => {
+    res.send({
+        message:'test'
+    })
+})
