b9a1332fcb3db98dae46c0ea4341cd4affc20b05
simonNolf <simon.nolf@gmail.com>
2022-03-17 16:33:40 +0100

    routage qui fonctionne

--- projet_dev3/src/App.js
+++ projet_dev3/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/navbar/index";
-import { BrowserRouter as Router, Routes } from "react-router-dom";
-import Home from "./pages";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/index";
 import About from "./pages/about";
 import Play from "./pages/team";
 import Creation from "./pages/creation";
@@ -11,10 +11,12 @@ function App() {
   return (
     <Router>
       <Navbar />
-      <Routes path="/" exact component={Home} />
-      <Routes path="/about" component={About} />
-      <Routes path="/creation" component={Creation} />
-      <Routes path="/play" component={Play} />
+      <Switch>
+        <Route path="/index" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/creation" component={Creation} />
+        <Route path="/play" component={Play} />
+      </Switch>
     </Router>
   );
 }
