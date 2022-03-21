020b1db55cdc82ae66a9a8e407b0be2df31aaf7e
MorganHolsters <he201896@students.ephec.be>
2022-03-21 07:18:49 +0100

    Frontpage and stylesheet

--- serveur/src/index.js
+++ serveur/src/index.js
@@ -4,12 +4,12 @@ const cors = require('cors');
 const morgan = require('morgan');
 var mysql = require('mysql2');
 
-var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'wassim',
-    password : 'wassim',
-    database : "gyt"
-  });
+ var connection = mysql.createConnection({
+     host     : 'localhost',
+     user     : 'wassim',
+     password : 'wassim',
+     database : "gyt"
+   });
 
 connection.connect();
 
@@ -18,7 +18,7 @@ const app = express();
 app.use(bodyParser.json())
 app.use(cors())
 app.use(morgan('common'))
-const port = process.env.port || 8080;
+const port = process.env.port || 3000;
 
 app.listen(port,() => console.log(`server started on port ${port}`));
 
