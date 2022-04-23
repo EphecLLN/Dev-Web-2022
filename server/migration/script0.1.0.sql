b33eeb96877dbd84d1a0a26ed78d5b24a8a7a876
simonNolf <simon.nolf@gmail.com>
2022-04-23 16:26:24 +0200

    Update script0.1.0.sql

--- server/migration/script0.1.0.sql
+++ server/migration/script0.1.0.sql
@@ -2,26 +2,6 @@
 CREATE DATABASE IF NOT EXISTS `site-hero` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `site-hero`;
 
--- Listage de la structure de la table site-hero. inventaire
-CREATE TABLE IF NOT EXISTS `inventaire` (
-  `id` mediumint NOT NULL AUTO_INCREMENT,
-  `objet` varchar(50) NOT NULL,
-  `nom` varchar(20) NOT NULL,
-  `id_scenario` mediumint NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk__scenario` (`id_scenario`),
-  CONSTRAINT `fk__scenario` FOREIGN KEY (`id_scenario`) REFERENCES `scenario` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- Listage de la structure de la table projet. scenario
-CREATE TABLE IF NOT EXISTS `scenario` (
-  `id` mediumint NOT NULL AUTO_INCREMENT,
-  `user_id` mediumint NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_user` (`user_id`),
-  CONSTRAINT `fk_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
 
 -- Listage de la structure de la table projet. users
 CREATE TABLE IF NOT EXISTS `users` (
@@ -33,5 +13,29 @@ CREATE TABLE IF NOT EXISTS `users` (
   KEY `fk_scenario` (`id_scénario`),
   CONSTRAINT `fk_scenario` FOREIGN KEY (`id_scénario`) REFERENCES `scenario` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- Listage de la structure de la table projet. scenario
+CREATE TABLE IF NOT EXISTS `scenario` (
+  `id` mediumint NOT NULL AUTO_INCREMENT,
+  `user_id` mediumint NOT NULL,
+  `texte` JSON NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_user` (`user_id`),
+  CONSTRAINT `fk_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=UTF8MB4_GENERAL_CI;
+
+
+-- Listage de la structure de la table site-hero. inventaire
+CREATE TABLE IF NOT EXISTS `inventaire` (
+  `id` mediumint NOT NULL AUTO_INCREMENT,
+  `objet` varchar(50) NOT NULL,
+  `nom` varchar(20) NOT NULL,
+  `id_scenario` mediumint NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk__scenario` (`id_scenario`),
+  CONSTRAINT `fk__scenario` FOREIGN KEY (`id_scenario`) REFERENCES `scenario` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 
 
