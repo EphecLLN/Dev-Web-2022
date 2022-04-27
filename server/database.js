89a54278c2917e0ef6a832ec566cd720367f37e8
Austreelis <dev@austreelis.net>
2022-04-27 17:05:22 +0200

    [database.js] use DB_{USER,PASSWORD}

--- server/database.js
+++ server/database.js
@@ -1,9 +1,10 @@
+const { DB_USER, DB_PASSWORD } = require("mysql");
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
   host     : 'localhost',
-  user     : '',
-  password : '',
+  user     : DB_USER,
+  password : DB_PASSWORD,
   database : 'site-hero'
 });
 
