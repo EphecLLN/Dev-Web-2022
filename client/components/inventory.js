75154a61934c69ec504712b83be0d6c6772583f7
simonNolf <simon.nolf@gmail.com>
2022-05-07 18:03:33 +0200

    [inventory] create component Inventory

---
+++ client/components/inventory.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+export const Inventory = ({inv, setInv}) => { 
+    return(  
+        <details className="dropdown col-2">
+            <summary className="button is-full-width">inventory</summary>
+                <div className="card is-full-width">
+                    {inv.map(({ id, objet, nom}, i) => (
+                        <p key={i+1}><a id={id} title={objet} onClick={(event) => {
+                            setInv(inv.filter(del => del.id != id))
+                            }}>
+                            {nom}</a>
+                        </p>
+                    ))}
+                </div>
+        </details>)
+    }
