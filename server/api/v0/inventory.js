65f5ac0fce47cc997b915e2ba170da8a350d0740
simonNolf <simon.nolf@gmail.com>
2022-05-08 08:24:14 +0200

    [estlint]

--- server/api/v0/inventory.js
+++ server/api/v0/inventory.js
@@ -4,15 +4,13 @@ const db = require("../../database")
 
 /* GET inventory */
 router.get("/:id", (req, res) => {
-    // eslint-disable-next-line function-paren-newline
-    db.connection.query(
-        // eslint-disable-next-line max-len
-        `SELECT id,objet,nom FROM inventory where id_scenario = ${db.connection.escape(
-        req.params.id
-      )};`, (err, rows) => {
-      if (err) throw err
-      res.json(rows)
-    })
+  db.connection.query(`SELECT id,objet,nom FROM inventory where id_scenario = 
+    ${db.connection.escape(
+    req.params.id
+  )};`, (err, rows) => {
+    if (err) throw err
+    res.json(rows)
   })
+})
 
-  module.exports = router 
+module.exports = router 
